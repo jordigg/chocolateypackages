e36a77590faed06c6f17871cb231fc2665ffffc3
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-03-09 13:36:10 -0400

    adom.1.1.1 Fixed GUI shims

--- packages/adom-classic/tools/chocolateyInstall.ps1
+++ packages/adom-classic/tools/chocolateyInstall.ps1
@@ -4,13 +4,16 @@ $url = "http://www.ancardia.com/download/old/1.1.1/adom_windows_1.1.1_beta4.zip"
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) "content"
 
-$link = "adom"
+$link = "Adom"
 $folder = "CommonPrograms"
 $target = Join-Path $content "Adom_winbeta4.exe"
 $description = "Ancient Domains of Mystery"
 
+. $tools\bins.ps1
 . $tools\shortcut.ps1
 
 Install-ChocolateyZipPackage $id $url $content
+
+New-GuiBin -Name $target
 
 New-Shortcut -Link $link -Target $target -SpecialFolder $folder -Description $description
