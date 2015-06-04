2f570e518bda297f96ae8d908e38a26fb16a56a2
Anthony Mastrean <anthony.mastrean@gmail.com>
2015-06-04 19:19:57 -0400

    adom.1.1.1.1

--- packages/adom-classic/tools/chocolateyInstall.ps1
+++ packages/adom-classic/tools/chocolateyInstall.ps1
@@ -1,22 +1,23 @@
-﻿$id = "adom-classic"
+﻿$id   = "adom-classic"
 $name = "Ancient Domains of Mystery (ADOM)"
-$url = "http://www.ancardia.com/download/old/1.1.1/adom_windows_1.1.1_beta4.zip"
+$url  = "http://www.ancardia.com/download/old/1.1.1/adom_windows_1.1.1_beta4.zip"
 
-$tools = Split-Path $MyInvocation.MyCommand.Definition
+$tools   = Split-Path $MyInvocation.MyCommand.Definition
 $content = Join-Path (Split-Path $tools) "content"
-$adom = Join-Path $content "Adom_winbeta4.exe"
+$adom    = Join-Path $content "Adom_winbeta4.exe"
+$batch   = Join-Path $content "adom.bat"
 
 . $tools\bins.ps1
 . $tools\shortcut.ps1
 
 Install-ChocolateyZipPackage $id $url $content
 
-try {
-  New-GuiBin -Name $adom
-  New-Shortcut -Link $name -Target $adom -SpecialFolder "CommonPrograms"
+@"
+@ECHO OFF
+START "$name" /D "%LOCALAPPDATA%" "$adom" %*
+"@ | Out-File $batch -encoding 'ASCII'
 
-  Write-ChocolateySuccess $id
-} catch {
-  Write-ChocolateyFailure $id $_.Exception.Message
-  throw
-}
+New-IgnoreBin -Name $adom
+
+New-Shortcut -Link $name -Target $batch -SpecialFolder "CommonPrograms" -Icon $adom
+New-Shortcut -Link $name -Target $batch -SpecialFolder "CommonDesktop" -Icon $adom
--- packages/adom-classic/tools/chocolateyUninstall.ps1
+++ packages/adom-classic/tools/chocolateyUninstall.ps1
@@ -1,4 +1,4 @@
-$id = "adom-classic"
+$id   = "adom-classic"
 $name = "Ancient Domains of Mystery (ADOM)"
 
 $tools = Split-Path $MyInvocation.MyCommand.Definition
@@ -6,3 +6,4 @@ $tools = Split-Path $MyInvocation.MyCommand.Definition
 . $tools\shortcut.ps1
 
 Remove-Shortcut -Link $name -SpecialFolder "CommonPrograms"
+Remove-Shortcut -Link $name -SpecialFolder "CommonDesktop"
